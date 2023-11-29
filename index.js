013468969f2d42ab1f3a36185631402139fe303f
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2023-11-29 16:09:36 +0600

    verify token and jwt api created

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config()
 
 const app = express();
-const port = 5000;
+const port =process.env.PORT || 5000;
 
 // Middlewares
 app.use(cors());
@@ -33,6 +35,38 @@ async function run() {
 }
 
 run().catch(console.dir);
+
+
+const verifyToken = async (req,res,next)=>{
+  const token = req.cookies?.token;
+  if(!token){
+    return res.status(401).send({message:'Unauthorized Access'})
+  }
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+    if(err){
+      return res.status(401).send({message:'Unauthorized Access'})
+    }
+    req.user =decoded;
+    next();
+  })
+}
+//auth related api
+
+app.post('/api/v1/jwt', async (req,res)=>{
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'});
+
+  res.cookie('token',token{
+    httpOnly:true,
+    secure:true,
+    sameSite: 'none'
+  })
+  .send({success:true})
+})
+app.post('/api/v1/logout', async(req,res)=>{
+  const user= req.body;
+  res.clearCookie('token', {maxAge:0}).send({success:true})
+})
 
 // Pets collection API
 const petsCollection = client.db('petMasterDB').collection('pets');
